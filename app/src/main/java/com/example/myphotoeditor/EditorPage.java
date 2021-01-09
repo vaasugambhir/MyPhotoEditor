9c532a8bc0fccb225e3e070932232aff27409ca6
Vaasuawesome7 <55234743+Vaasuawesome7@users.noreply.github.com>
2021-01-09 14:27:10 +0530

    Custom ImageView -added

--- app/src/main/java/com/example/myphotoeditor/EditorPage.java
+++ app/src/main/java/com/example/myphotoeditor/EditorPage.java
@@ -14,8 +14,6 @@ import android.transition.Fade;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
